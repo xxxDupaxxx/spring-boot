45e92c1e0380f3a01a03fb383191562f7a0b22b3
xxxDupaxxx <1kamil.orlowski@gmail.com>
2019-04-16 07:48:13 -0500

    Added logging(by default root is INFO)

--- src/main/java/com/orlowski/springbootintroduction/controller/BaseController.java
+++ src/main/java/com/orlowski/springbootintroduction/controller/BaseController.java
@@ -1,14 +1,23 @@
 package com.orlowski.springbootintroduction.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
+    Logger logger = LoggerFactory.getLogger(BaseController.class);
 
     @RequestMapping(value = "/my/custom/path/summary", method = RequestMethod.GET)
     String getSomeSummary() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
         return "some_random_text";
     }
 }
