357a40dfec299a90e341a708b32cfcb146e629d9
xxxDupaxxx <1kamil.orlowski@gmail.com>
2019-04-16 07:33:42 -0500

    Added configuration

--- src/main/java/com/orlowski/springbootintroduction/SpringBootIntroductionApplication.java
+++ src/main/java/com/orlowski/springbootintroduction/SpringBootIntroductionApplication.java
@@ -1,12 +1,15 @@
 package com.orlowski.springbootintroduction;
 
+import com.orlowski.springbootintroduction.configuration.MyConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({MyConfiguration.class})
 public class SpringBootIntroductionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootIntroductionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroductionApplication.class, args);
+    }
 }
---
+++ src/main/java/com/orlowski/springbootintroduction/configuration/MyConfiguration.java
@@ -0,0 +1,7 @@
+package com.orlowski.springbootintroduction.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+}
